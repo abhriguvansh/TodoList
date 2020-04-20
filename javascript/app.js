72cbc5eceed371eac41866d18c8c092e1537d200
Aman <abhriguvansh99@gmail.com>
2020-04-20 12:48:44 -0700

    added check and delete functionality for items in todo list

--- javascript/app.js
+++ javascript/app.js
@@ -3,13 +3,14 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteAndCheck);
 
 function addTodo(event) {
   event.preventDefault(); //prevent form from submitting
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
   const newTodo = document.createElement('li');
-  newTodo.innerText = 'Test';
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
   todoDiv.appendChild(newTodo);
   const completedButton = document.createElement('button');
@@ -21,4 +22,17 @@ function addTodo(event) {
   trashButton.classList.add('trash-btn');
   todoDiv.appendChild(trashButton);
   todoList.appendChild(todoDiv);
+  todoInput.value = '';
+}
+
+function deleteAndCheck(event) {
+  const item = event.target;
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    todo.remove();
+  }
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
 }
